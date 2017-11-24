5318fa8466631443380d55294a8b8152bf5bfd89
HrafnkellTorri <Kallinnhannhrabbi@hotmail.com>
2017-11-24 22:54:36 +0000

    Add files via upload

--- engine.js
+++ engine.js
@@ -1,9 +1,12 @@
 "use strict";
+var alert;
+
 
 var canvas = document.getElementById("myCanvas");
+var textColor = randomColor();
 var ctx = canvas.getContext("2d");
 
-var ballRadius = 30;
+var difficulty = 6;
 
 var playerHeight = 40;
 var playerWidth = 40;
@@ -12,85 +15,174 @@ var playerY = canvas.height-playerHeight;
 
 var rightPressed = false;
 var leftPressed = false;
-var upPressed = false;
+var upPressed = false; 
 var downPressed = false;
 
+var player1color = randomColor(); 
+var rawplayer1Score = 0;
+var trueplayer1Score = 0;
 
 var Ball = {
 
-    create : function (ball_x, ball_y,ball_dx,ball_dy,ball_color)
+    create : function (ball_x, ball_y,ball_dx,ball_dy,ball_rad,ball_color)
     {
         var newBall = Object.create(this);
         newBall.x = ball_x;
         newBall.y = ball_y;
         newBall.dx = ball_dx;
         newBall.dy = ball_dy;
+        newBall.ballRadius = ball_rad;
         newBall.color = ball_color;
         return newBall;
     },
 
     drawBall : function() {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, ballRadius, 0, Math.PI*2);
+    ctx.arc(this.x, this.y, this.ballRadius, 0, Math.PI*2);
     ctx.fillStyle = this.color;
     ctx.fill();
+    ctx.arc(this.x, this.y, this.ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "black";
+    ctx.stroke();
     ctx.closePath();
     },
 
     checkboundaries : function() {
-        if(this.x + this.dx > canvas.width-ballRadius || this.x + this.dx < ballRadius) {
+        if(this.x + this.dx > canvas.width-this.ballRadius || this.x + this.dx < this.ballRadius) {
             this.dx = -this.dx;
-            this.y += 1;
+            this.ballRadius += 1;
+            
         }
-
-        if(this.y + this.dy > canvas.height-ballRadius || this.y + this.dy < ballRadius) {
+        if(this.y + this.dy > canvas.height-this.ballRadius || this.y + this.dy < this.ballRadius) {
             this.dy = -this.dy;
-            this.x -= 1;
+            this.ballRadius += 0.5;
+            
         }
+        if (PlayerAndCircleCollision(this)) 
+        {
 
-
+            setTimeout(alert("Hello "), 15000);
+            location.reload();
+        } 
+        else 
+        {
+            ctx.fillStyle = "skyblue";
+        }
         this.x += this.dx;
         this.y += this.dy;
-
     }
 };
 
-var ball1 = Ball.create(500,100,1,3,"red");
-var ball2 = Ball.create(111,250,5,2,"green");
+var ball1 = Ball.create(Math.random() * 500 + 20,Math.random() * 500 + 20,-difficulty,difficulty,30,randomColor());
+var ball2 = Ball.create(Math.random() * 500 + 20,Math.random() * 500 + 20,difficulty,difficulty,30,randomColor());
+var ball3 = Ball.create(Math.random() * 500 + 20,Math.random() * 500 + 20,3,3,30,randomColor());
+var ball4 = Ball.create(Math.random() * 500 + 20,Math.random() * 500 + 20,3,-3,30,randomColor());
+var ball5 = Ball.create(Math.random() * 500 + 20,Math.random() * 500 + 20,difficulty,difficulty,30,randomColor());
+var ball6 = Ball.create(Math.random() * 500 + 20,Math.random() * 500 + 20,difficulty,difficulty,30,randomColor());
 
-function draw() {
+function draw() 
+{
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ball1.checkboundaries();
-    ball2.checkboundaries();
-    ball1.drawBall();
-    ball2.drawBall();
     drawplayer();
     playerControler();
+    scoreDrawingAndDifficultySettings();
+    spawnBalls();
 }
-
 
 function drawplayer() {
     ctx.beginPath();
     ctx.rect(playerX, playerY, playerWidth, playerHeight);
-    ctx.fillStyle = "#9925EE";
+    ctx.fillStyle = player1color;
     ctx.fill();
+    ctx.rect(playerX, playerY, playerWidth, playerHeight);
+    ctx.fillStyle = "black";
+    ctx.lineWidth = 3.5;
+    ctx.stroke();
     ctx.closePath();
+}
+
+function PlayerAndCircleCollision(ball) {
+    var distX = Math.abs(ball.x - playerX - playerWidth / 2.2);
+    var distY = Math.abs(ball.y - playerY - playerHeight / 2.2);
+
+    if (distX > (playerWidth / 2 + ball.ballRadius)) 
+    {
+        return false;
+    }
+    if (distY > (playerHeight / 2 + ball.ballRadius)) 
+    {
+        return false;
+    }
+    if (distX <= (playerWidth / 2)) {
+        return true;
+    }
+    if (distY  <= (playerHeight / 2)) {
+        return true;
+    }
+
+    var rx = distX - playerWidth / 2;
+    var ry = distY - playerHeight / 2;
+    return (rx * rx + ry * ry <= (ball.ballRadius * ball.ballRadius));
 }
 
 function playerControler()
 {
-    if(rightPressed && playerX < canvas.width - playerWidth - 3) {
-        playerX += 7;
+    if (rightPressed || leftPressed || upPressed || downPressed) 
+    {
+        
+        if(rightPressed && playerX < canvas.width - playerWidth - 3) {
+            playerX += 14;
+        }
+        else if(leftPressed && playerX > 2) {
+            playerX -= 14;
+        }
+        else if(upPressed && playerY > 4) {
+            playerY -= 14;
+        }
+        else if(downPressed && playerY < canvas.height - 3 - playerHeight) {
+            playerY += 14;
+        }
+
+        score(); 
     }
-    else if(leftPressed && playerX > 2) {
-        playerX -= 7;
+
+}
+
+function spawnBalls()
+{
+    ball1.checkboundaries();
+    ball2.checkboundaries();
+    ball1.drawBall();
+    ball2.drawBall();
+    if(trueplayer1Score > 4)
+    {
+        ball3.drawBall();
+        ball3.checkboundaries();
     }
-    else if(upPressed && playerY > 4) {
-        playerY -= 7;
+    if(trueplayer1Score > 9)
+    {
+        ball4.drawBall();
+        ball4.checkboundaries();
     }
-    else if(downPressed && playerY < canvas.height - 3 - playerHeight) {
-        playerY += 7;
+    if(trueplayer1Score > 14)
+    {
+        ball5.drawBall();
+        ball5.checkboundaries();
     }
+    if(trueplayer1Score > 19)
+    {
+        ball6.drawBall();
+        ball6.checkboundaries();
+    }
+}
+
+function randomColor() {
+  var letters = '0123456789ACDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 function keyDownHandler(e) 
@@ -125,7 +217,41 @@ function keyUpHandler(e)
     }
 }
 
+function scoreDrawingAndDifficultySettings()
+{
+    ctx.font = "30px Arial";
+    ctx.fillStyle = textColor;
+    var num = Math.round(rawplayer1Score / 100);
+    ctx.fillText("Points: " + num + " " + difficulty + " ",10,50);
+    if(num > 5)
+    {
+        difficulty = 10;
+        trueplayer1Score = num;
+    }
+    if(num > 10)
+    {
+        difficulty = 20;
+        trueplayer1Score = num;
+    }
+    if(num > 15)
+    {
+        difficulty = 30;
+        trueplayer1Score = num;
+    }
+    if(num > 20)
+    {
+        difficulty = 45;
+        trueplayer1Score = num;
+    }
+}
+
+function score()
+{
+    rawplayer1Score++;
+}
+
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-setInterval(draw, 10);
+setInterval(draw, 20);
